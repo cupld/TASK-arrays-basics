57922f9f4416e83636a6e7508190556fa10018fa
Ahmed Dashti <cupld@hotmail.com>
2021-12-23 09:51:46 +0300

    array.js

---
+++ array.js
@@ -0,0 +1,16 @@
+let epmtyarray =[];
+let hobbies =['martial arts','video games','car modifications','reading','hobby5','hobby6']
+let numbers =['5','3','1','7','9']
+
+// console.log(numbers[1]);
+// console.log(hobbies[5])
+// hobbies.push("fake hobby");
+// console.log(hobbies[6]);
+// console.log(hobbies.length-1)
+// console.log(hobbies[hobbies.length-1]);
+// hobbies.pop();
+// hobbies.pop();
+// console.log(hobbies.length);
+// hobbies.pop(hobbies.length-1)
+
+// no elements in hobbies
